d0dacf4d88f7cc071c8c632d4b6f577de69eedca
Veera Kishon Kumar Mucherla <veerakishon.mucherla@gmail.com>
2018-05-31 10:04:16 -0400

    bypassed warnings.

--- src/com/kishon/datepractise/DateAPIDemo.java
+++ src/com/kishon/datepractise/DateAPIDemo.java
@@ -10,6 +10,6 @@ public static void main(String... args) {
 	Date d1 = new Date(2018-1900,04,30);
 	System.out.println(d1);
 	Date d2 = new Date(d.getTime());
-	System.out.println(d.getTime());	
+	System.out.println(d2.getTime());	
 }
 }
